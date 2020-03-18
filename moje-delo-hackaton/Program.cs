98f6fb209b72003df7cbe0f122183bae983d5393
Marko <marko.culibrk@gmail.com>
2020-03-18 23:16:23 +0100

    Zapiski; Branje vhodnih podatkov

--- moje-delo-hackaton/Program.cs
+++ moje-delo-hackaton/Program.cs
@@ -1,29 +1,36 @@
 ﻿using System;
+using System.IO;
 
 /*
-širina višina besedilo
-Vhod:
-20 6 led display
-100 20 led display 2020
-10 20 MUST BE ABLE TO DISPLAY
-55 25 Can you hack
-100 20 display product text
-
-Izhod:
-2
-9
-1
-8
-8
-
-    Hitro kaj lahko testiram:
-    - Maksimalna velikost pisave glede na ekran:
-    -- Vzame se min vrednost dimenzij ekrana -> min(širina, višina)
-    - Maksimalna velikost pisave glede na najdaljšo besedo
-    -- Najdem dolžino najdaljše besede, širino ekrana delim z to dolžino
-
-
-
+ * širina višina besedilo
+ * 
+ * Vhod:
+ * 20 6 led display
+ * 100 20 led display 2020
+ * 10 20 MUST BE ABLE TO DISPLAY
+ * 55 25 Can you hack
+ * 100 20 display product text
+ * 
+ * Izhod:
+ * 2
+ * 9
+ * 1
+ * 8
+ * 8
+ * 
+ * 20 100 display product text => 2
+ * 
+ * Hitro kaj lahko testiram:
+ * - maxE = Maksimalna velikost pisave glede na ekran:
+ * -- Vzame se min vrednost dimenzij ekrana -> min(širina, višina)
+ * - maxB = Maksimalna velikost pisave glede na najdaljšo besedo
+ * -- Najdem dolžino najdaljše besede, širino ekrana delim z to dolžino
+ *
+ * - maxD(stV) = Maksimalna velikost pisave glede na dolžino besedila, število besed in trenutno število vrstic:
+ * -- Seštej dolžine vseh besed
+ * -- Prištej število presledkov: <št. presledkov> = <št. vseh besed> - 1 - ( <št. vrstic> - 1 )
+ * -- Površino ekrana (širina * št. vrstic) deli z prejšnim rezultatom.
+ * 
  */
 
 namespace moje_delo_hackaton
@@ -32,7 +39,26 @@ namespace moje_delo_hackaton
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var inputPath = "vhodi.txt";
+
+            if (!File.Exists(inputPath))
+            {
+                Console.WriteLine($"Datoteka {inputPath} ne obstaja");
+                return;
+            }
+
+            var lines = File.ReadAllLines(inputPath);
+            foreach (var line in lines)
+            {
+                var vseBesede = line.Split(' ');
+                var besede = new string[vseBesede.Length - 2];
+                var j = 0;
+                for (int i = 2; i < vseBesede.Length; i++)
+                {
+                    besede[j] = vseBesede[i];
+                }
+                Console.WriteLine($"Širina:{besede[0]}; Višina:{besede[1]}; {line}");
+            }
         }
     }
 }

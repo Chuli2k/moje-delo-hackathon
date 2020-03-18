2bdc13cf86928fc72c5ad398af534411e7d6fadb
Marko <marko.culibrk@gmail.com>
2020-03-18 22:16:30 +0100

    Add project files.

---
+++ moje-delo-hackaton/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+/*
+širina višina besedilo
+Vhod:
+20 6 led display
+100 20 led display 2020
+10 20 MUST BE ABLE TO DISPLAY
+55 25 Can you hack
+100 20 display product text
+
+Izhod:
+2
+9
+1
+8
+8
+
+    Hitro kaj lahko testiram:
+    - Maksimalna velikost pisave glede na ekran:
+    -- Vzame se min vrednost dimenzij ekrana -> min(širina, višina)
+    - Maksimalna velikost pisave glede na najdaljšo besedo
+    -- Najdem dolžino najdaljše besede, širino ekrana delim z to dolžino
+
+
+
+ */
+
+namespace moje_delo_hackaton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

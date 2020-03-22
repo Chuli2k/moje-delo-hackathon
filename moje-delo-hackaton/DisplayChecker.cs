4adc659610039edfae3dc83977e5958b214e92cf
Marko <marko.culibrk@gmail.com>
2020-03-22 18:33:04 +0100

    Pripravil program za oddajo

---
+++ moje-delo-hackaton/DisplayChecker.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace moje_delo_hackaton
+{
+    public class DisplayChecker
+    {
+        public int DisplayWidth { get; private set; }
+        public int DisplayHeight { get; private set; }
+        public IList<int> WordListSizes { get; private set; }
+
+        //Naredim spremenljivko, da ne računam vsakič sumo...
+        private int sumWordListSizes = 0;
+
+        public int GetMaxFontSize(int displayWidth, int displayHeight, IList<int> wordListSizes)
+        {
+            //Init
+            DisplayWidth = displayWidth;
+            DisplayHeight = displayHeight;
+            WordListSizes = wordListSizes;
+            sumWordListSizes = WordListSizes.Sum();
+
+            //Najdi min stranico ekrana in nastavi to kot navečjo možno velikost font-a
+            var maxFontSize = DisplayWidth < DisplayHeight ? DisplayWidth : DisplayHeight;
+
+            //Najdi max velikost ekrana glede na najdaljšo besedo, če je manjša kot prej, jo nastavi
+            int maxfontSizeByMaxWordLength = DisplayWidth / WordListSizes.Max(); //Celi del deljenja
+            if (maxfontSizeByMaxWordLength < maxFontSize) maxFontSize = maxfontSizeByMaxWordLength;
+
+            while (maxFontSize > 0)
+            {
+                if (CheckFontSize(maxFontSize))
+                    return maxFontSize;
+
+                maxFontSize--;
+            }
+
+            return maxFontSize;
+        }
+
+        /// <summary>
+        /// Preveri, če se podana velikost pisave da prikazat za dimenzije ekrana in velikosti besed
+        /// </summary>
+        /// <returns></returns>
+        private bool CheckFontSize(int maxFontSize)
+        {
+            //Spremenjljivki za trenutni pozicijo
+            var x = 0; //pixel na horizontali
+            var y = 1; //V kateri vrstici sem...
+            var nrSpaces = -1; //-1 ker, če je samo ena beseda, pomeni, da ni presledkov...
+
+            //Normalizirana širina in višina glede na velikost pisave.
+            int normWidth = DisplayWidth / maxFontSize;
+            int normHeight = DisplayHeight / maxFontSize;
+
+            //Preverjanje, da je na voljo dovolj prostora za vse znake
+            int area = normWidth * normHeight;
+            int numChars = sumWordListSizes + WordListSizes.Count - normHeight;
+            if (area < numChars) return false;
+
+            for (int i = 0; i < WordListSizes.Count; i++)
+            {
+                x += WordListSizes[i];
+                nrSpaces++;
+
+                //Preveri, če je prekoračena širina. Če ja idi v novo vrstico
+                if ((x + nrSpaces) > normWidth)
+                {
+                    //Idi v novo vrstico
+                    y++;
+
+                    if (y > normHeight)
+                    {
+                        //prekoračil sem število vrstic...
+                        return false;
+                    }
+
+                    //Resetiraj x pozicijo
+                    x = 0;
+
+                    //resetiraj trenutno besedo, da se uporabi v novi vrstici
+                    i--;
+
+                    //Resetiraj število presledkov
+                    nrSpaces = -1;
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- moje-delo-hackaton/Program.cs
+++ moje-delo-hackaton/Program.cs
@@ -3,15 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.IO;
 
-/*
- * To še preveri:
- * - maxD(stV) = Maksimalna velikost pisave glede na dolžino besedila, število besed in trenutno število vrstic:
- * -- Seštej dolžine vseh besed
- * -- Prištej število presledkov: <št. presledkov> = <št. vseh besed> - 1 - ( <št. vrstic> - 1 )
- * -- Površino ekrana (širina * št. vrstic) deli z prejšnim rezultatom.
- * 
- */
-
 namespace moje_delo_hackaton
 {
     class Program
@@ -28,19 +19,25 @@ namespace moje_delo_hackaton
             }
 
             var lines = File.ReadAllLines(inputFilePath);
+            var results = new List<int>();
+            var checker = new DisplayChecker();
+            Console.WriteLine("Vhod:");
             foreach (var line in lines)
             {
-                var besede = new List<string>(line.Split(' '));
-                besede.RemoveAt(0);
-                besede.RemoveAt(0);
-                var besedeLength = besede.Select(b => b.Length).ToList();
-                Console.WriteLine($"{line} ({besedeLength.Aggregate("", (prev, curr) => prev + " " + curr.ToString())})");
-
                 var (width, height, wordLengths) = ParseLine(line);
-                var f = GetMaxFontSize(width, height, wordLengths.ToList());
-                Console.WriteLine($"Result:{f}");
-                Console.WriteLine();
+                //Console.WriteLine($"{line} ({wordLengths.Aggregate("", (prev, curr) => prev + " " + curr.ToString())})");
+                Console.WriteLine(line);
+                results.Add(checker.GetMaxFontSize(width, height, wordLengths));
             }
+
+            Console.WriteLine();
+            Console.WriteLine("Izhod:");
+            foreach (var result in results)
+            {
+                Console.WriteLine(result);
+            }
+
+            Console.ReadKey();
         }
 
         /// <summary>
@@ -48,7 +45,7 @@ namespace moje_delo_hackaton
         /// </summary>
         /// <param name="line"></param>
         /// <returns>(width, height, wordLengths[])</returns>
-        private static (int, int, IEnumerable<int>) ParseLine(string line)
+        private static (int, int, IList<int>) ParseLine(string line)
         {
             var vseBesede = line.Split(' ');
             var besede = new List<string>(vseBesede);
@@ -57,84 +54,6 @@ namespace moje_delo_hackaton
             var besedeLength = besede.Select(b => b.Length).ToList();
 
             return (int.Parse(vseBesede[0]), int.Parse(vseBesede[1]), besedeLength);
-        }
-
-        private static int GetMaxFontSize(int width, int height, IList<int> wordLengths)
-        {
-            //Najdi min stranico ekrana in nastavi to kot navečjo možno velikost font-a
-            var maxFontSize = width < height ? width : height;
-
-            //Najdi max velikost ekrana glede na najdaljšo besedo, če je manjša kot prej, jo nastavi
-            int maxfontSizeByMaxWordLength = width / wordLengths.Max(); //Celi del deljenja
-            if (maxfontSizeByMaxWordLength < maxFontSize) maxFontSize = maxfontSizeByMaxWordLength;
-
-            Console.WriteLine($"init maxFontSize:{maxFontSize}");
-
-            while(maxFontSize > 0)
-            {
-                if (CheckFontSize(width, height, wordLengths, maxFontSize))
-                    return maxFontSize;
-
-                maxFontSize--;
-            }
-
-            return maxFontSize;
-        }
-
-        /// <summary>
-        /// Preveri, če se podana velikost pisave da prikazat za podane dimenzije ekrana in velikosti besed
-        /// </summary>
-        /// <param name="width"></param>
-        /// <param name="height"></param>
-        /// <param name="wordLengths"></param>
-        /// <param name="maxFontSize"></param>
-        /// <returns></returns>
-        private static bool CheckFontSize(int width, int height, IList<int> wordLengths, int maxFontSize)
-        {
-            //Spremenjljivki za trenutni pozicijo
-            var x = 0; //pixel na horizontali
-            var y = 0; //V kateri vrstici sem...
-            var nrSpaces = -1; //-1 ker, če je samo ena beseda, pomeni, da ni presledkov...
-            
-            //Normalizirana širina in višina glede na velikost pisave.
-            int normWidth = width / maxFontSize;
-            int normHeight = height / maxFontSize;
-
-            //Preverjanje, da je na voljo dovolj prostora za vse znake
-            int area = normWidth * normHeight;
-            int numChars = wordLengths.Sum() + wordLengths.Count - normHeight; //TODO: sum in count bi lahko ven prestavil.
-            if (area < numChars) return false;
-
-            for (int i = 0; i < wordLengths.Count; i++)
-            {
-                x += wordLengths[i];
-                nrSpaces++;
-
-                //Preveri, če je prekoračena širina. Če ja idi v novo vrstico
-                if ((x + nrSpaces) > normWidth)
-                {
-                    //Idi v novo vrstico
-                    y++;
-
-                    if (y == normHeight)
-                    {
-                        //prekoračil sem število vrstic...
-                        return false;
-                    }
-
-                    //Resetiraj x pozicijo
-                    x = 0;
-
-                    //resetiraj trenutno besedo, da se uporabi v novi vrstici
-                    //TODO: Preveri kaj naredit, če je i==0. In kdaj se lahko to zgodi? Praviloma se ne bi smelo...
-                    i--; 
-                    
-                    //Resetiraj število presledkov
-                    nrSpaces = -1;
-                }
-            }
-
-            return true;
         }
     }
 }
